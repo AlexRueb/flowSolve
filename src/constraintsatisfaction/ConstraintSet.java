62f8ba7d48591b3cc1e7ddd633b3e253375ce509
AlexRueb <alexrueb668@gmail.com>
2018-10-23 23:37:10 -0600

    little changes

--- src/constraintsatisfaction/ConstraintSet.java
+++ src/constraintsatisfaction/ConstraintSet.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package constraintsatisfaction;
 
-/**
- *
- * @author alex
- */
 class ConstraintSet {
     //this is where we will have all of our constraints
     //1 --> each non-startNode will have exactly 2 of the same color neighbors
