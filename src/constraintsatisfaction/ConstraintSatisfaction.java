f1f04f93e82f870519654efb9af9a2ff3e1beb5e
AlexRueb <alexrueb668@gmail.com>
2018-11-01 17:35:46 -0600

    garbled code

--- src/constraintsatisfaction/ConstraintSatisfaction.java
+++ src/constraintsatisfaction/ConstraintSatisfaction.java
@@ -4,23 +4,19 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  *
  * @author alex
  */
-public class ConstraintSatisfaction {
+class ConstraintSatisfaction {
 
-    
-    public ConstraintSet constraint = new ConstraintSet();
-    
     public static void main(String[] args) throws IOException {
         // TODO code application logic here
         String[][] board = import_maze("src/constraintsatisfaction/5x5maze.txt");
-        solveMaze(board, 1);
-        solveMaze(board, 2);
+        Maze maze = new Maze();
+        maze.solveMaze(board, 1);
+        maze.solveMaze(board, 2);
 //        board = import_maze("src/constraintsatisfaction/7x7maze.txt");
 //        solveMaze(board);
 //        board = import_maze("src/constraintsatisfaction/8x8maze.txt");
@@ -36,10 +32,13 @@ public class ConstraintSatisfaction {
 
     }
 
-    public static String[][] import_maze(String f) throws IOException {
+    static String[][] import_maze(String f) throws IOException {
         int boardX = 100;
         int boardY = 100;
-        //Im too lazy to make a dynamically sized 2D array
+        //rewrite to make it read in the first line, then set boardX/Y to length of string
+        //then make array
+        //then assign all values
+        //assumes that board is a square
         switch (f) {
             case "src/constraintsatisfaction/5x5maze.txt":
                 boardX = 5;
@@ -80,6 +79,7 @@ public class ConstraintSatisfaction {
                 String line = b.readLine();
                 char[] charArr = line.toCharArray();
                 String[] boardArr = new String[line.length()];
+
                 for (int q = 0; q < line.length(); q++) {
                     boardArr[q] = String.valueOf(charArr[q]);
                 }
@@ -92,107 +92,13 @@ public class ConstraintSatisfaction {
         return board;
 
     }
+
     
-    //Choice is whether to do the smart way (forward checking) and 
-    //dumb way (no forward checking)
-    public static void solveMaze(String[][] board, int choice) {
-        Node[][] nodeArr = setup(board);
-
-        //If smart choice
-        if (choice == 1) {
-            System.out.println("smart");
-            nodeArr = smartSolve(nodeArr);
-            printBoard(nodeArr);
-            
-            //If dumb choice
-        } else {
-            System.out.println("dumb");
-            dumbSolve(nodeArr);
-            printBoard(nodeArr);
-        }
-    }
     
-    //This method converts the String array to a Node array
-    //and assigns each Node with the information necessary to solve the board
-    public static Node[][] setup(String[][] board){
-        List<String> colors = new ArrayList<>();
 
-        //Creating node array
-        int x = board.length;
-        int y = board.length;
-        Node[][] nodeArr = new Node[x][y];
-
-        for (int i = 0; i < y; i++) {
-            
-            for (int j = 0; j < x; j++) {
-                //Makes a new node that represents the space of the board
-                nodeArr[i][j] = new Node();
-                //If there is a color initially
-                if (!board[i][j].equals("_")) {
-                    //It's a start node
-                    nodeArr[i][j].startNode = true;
-                    //dynamically keeping track of the amount of colors
-                    if (colors.indexOf(board[i][j]) == -1) {
-                        colors.add(board[i][j]);
-                    }
-                }
-                
-                nodeArr[i][j].color = board[i][j];
-                nodeArr[i][j].xCord = j;
-                nodeArr[i][j].yCord = i;
-            }
-        }
-
-        //assign neighbors
-        for (int i = 0; i < y; i++) {
-            for (int j = 0; j < x; j++) {
-
-                //if not top row
-                if (i != 0) {
-                    nodeArr[i][j].up = nodeArr[i - 1][j];
-                }
-                //if not left side
-                if (j != 0) {
-                    nodeArr[i][j].left = nodeArr[i][j - 1];
-                }
-                //if not bottom row
-                if (i != x - 1) {
-                    nodeArr[i][j].down = nodeArr[i + 1][j];
-                }
-                //if not right side
-                if (j != y - 1) {
-                    nodeArr[i][j].right = nodeArr[i][j + 1];
-                }
-                //adds all of the colors to the nodes possible colors
-                if (nodeArr[i][j].color.equals("_")) {
-                    nodeArr[i][j].colors.addAll(colors);
-                }
-            }
-        }
-        return nodeArr;
-    }
     
-    public static Node[][] smartSolve(Node[][] board){
-        
-        return board;
-    }
     
-    public static Node[][] dumbSolve(Node[][] board){
-        
-        return board;
-    }
 
-    public static void printBoard(Node[][] board) {
-        System.out.println(board.length);
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board.length; j++) {
-                System.out.print(board[i][j].color);
-            }
-            System.out.println("");
-        }
-        System.out.println(" ");
-        System.out.println(" ");
-    }
     
 
 }
--- src/constraintsatisfaction/Node.java
+++ src/constraintsatisfaction/Node.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 class Node {
+    //each node is a variable
+    
+    //where the node is
     public int xCord;
     public int yCord;
     public String color;
@@ -13,9 +16,21 @@ class Node {
     public Node down;
     public boolean startNode = false;
     public int sameCol = 0;
-    public List<String> colors= new ArrayList<>();
     
+    //domain
+    public List<String> colors= new ArrayList<>();
+    public boolean solved;
+    
+    //these are the distances to goal
+    public int xDist;
+    public int yDist;
+    
+    //updates the possible colors that a node can have --> inference
     public boolean checkPossible(char color){
         return true;
+    }
+    public void updateGoal(int x, int y){
+        xDist = x - xCord;
+        yDist = y - yCord;
     }
 }
