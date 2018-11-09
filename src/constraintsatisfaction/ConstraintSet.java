cff7a5fbb1d227fb2cace72677e949f7b689f1fd
DillTice <alexrueb668@gmail.com>
2018-11-08 20:20:01 -0700

    Recursion logic wrong

--- src/constraintsatisfaction/ConstraintSet.java
+++ src/constraintsatisfaction/ConstraintSet.java
@@ -6,44 +6,47 @@ class ConstraintSet {
     //1 --> each non-startNode will have exactly 2 of the same color neighbors
     //2 --> each Node will have a color --> no "_"
     //3 --> each origin node has exactly 1 of the same color neighbors
-    
-    
     //always run immediately after assigning a previously blank node a new color
-    public boolean doesZig(Node x) {
-        int same = 0;
-        if (x.up.color.equals(x.color)) {
-            same++;
+    public boolean doesZig(Node[][] x) {
+        for (Node[] row : x) {
+            for (Node n : row) {
+                int same = 0;
+                if (n.up != null && n.up.color.equals(n.color)) {
+                    same++;
+                }
+                if (n.down != null && n.down.color.equals(n.color)) {
+                    same++;
+                }
+                if (n.left != null && n.left.color.equals(n.color)) {
+                    same++;
+                }
+                if (n.right != null && n.right.color.equals(n.color)) {
+                    same++;
+                }
+                return same > 2;
+            }
         }
-        if (x.down.color.equals(x.color)) {
-            same++;
-        }
-        if (x.left.color.equals(x.color)) {
-            same++;
-        }
-        if (x.right.color.equals(x.color)) {
-            same++;
-        }
-        return same > 2;
+        return false;
     }
-    
-    public boolean checkOrigin(Node[][] board){
-        for(Node[] row : board){
-            for(Node n : row){
-                if(n.startNode){
+
+    public boolean checkOrigin(Node[][] board) {
+        for (Node[] row : board) {
+            for (Node n : row) {
+                if (n.startNode) {
                     int same = 0;
-                    if (n.up.color.equals(n.color)) {
+                    if (n.up != null && n.up.color.equals(n.color)) {
                         same++;
                     }
-                    if (n.down.color.equals(n.color)) {
+                    if (n.down != null && n.down.color.equals(n.color)) {
                         same++;
                     }
-                    if (n.left.color.equals(n.color)) {
+                    if (n.left != null && n.left.color.equals(n.color)) {
                         same++;
                     }
-                    if (n.right.color.equals(n.color)) {
+                    if (n.right != null && n.right.color.equals(n.color)) {
                         same++;
                     }
-                    if(same > 1){
+                    if (same > 1) {
                         return false;
                     }
                 }
@@ -51,15 +54,21 @@ class ConstraintSet {
         }
         return true;
     }
-    
-    public boolean notBroken(Node[][] board){
-        return true;
+
+    public boolean notBroken(Node[][] board) {
+        if (!doesZig(board)) {
+            return (checkOrigin(board));
+        }
+        return false;
     }
-    
-    public boolean isComplete(Node[][] board){
-        for(Node[] row : board){
-            for(Node n : row){
-                if(n.color.equals("_")){
+
+    public boolean isComplete(Node[][] board) {
+        if(!notBroken(board)){
+            return false;
+        }
+        for (Node[] row : board) {
+            for (Node n : row) {
+                if (n.color.equals("_")) {
                     return false;
                 }
             }
