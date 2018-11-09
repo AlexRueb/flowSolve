fa76b65f57f11f28e1f1f983fd87bd7735a5a09e
dtice <dtice@localhost>
2018-11-08 19:57:33 -0700

    hi alex

--- src/constraintsatisfaction/ConstraintSet.java
+++ src/constraintsatisfaction/ConstraintSet.java
@@ -5,7 +5,7 @@ class ConstraintSet {
     //this is where we will have all of our constraints
     //1 --> each non-startNode will have exactly 2 of the same color neighbors
     //2 --> each Node will have a color --> no "_"
-    //3 --> 
+    //3 --> each origin node has exactly 1 of the same color neighbors
     
     
     //always run immediately after assigning a previously blank node a new color
@@ -26,11 +26,40 @@ class ConstraintSet {
         return same > 1;
     }
     
+    public boolean checkOrigin(Node[][] board){
+        for(Node[] row : board){
+            for(Node n : row){
+                int same = 0;
+                if (n.up.color.equals(n.color)) {
+                    same++;
+                }
+                if (n.down.color.equals(n.color)) {
+                    same++;
+                }
+                if (n.left.color.equals(n.color)) {
+                    same++;
+                }
+                if (n.right.color.equals(n.color)) {
+                    same++;
+                }
+                return same <= 1;
+            }
+        }
+        return true;
+    }
+    
     public boolean notBroken(Node[][] board){
         return true;
     }
     
     public boolean isComplete(Node[][] board){
-        return false;
+        for(Node[] row : board){
+            for(Node n : row){
+                if(n.color.equals("_")){
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
